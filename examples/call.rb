79ab261634a35487670b8d42673adaa1276e0df7
Nando Vieira <fnando.vieira@gmail.com>
2016-03-18 09:34:33 -0300

    Add Signal.call example.

---
+++ examples/call.rb
@@ -0,0 +1,20 @@
+$:.unshift File.expand_path('../../lib', __FILE__)
+require 'signal'
+
+class Contact
+  include Signal.call
+
+  attr_reader :name, :email
+
+  def initialize(name, email)
+    @name, @email = name, email
+  end
+
+  def call
+    emit(:output, self)
+  end
+end
+
+Contact.call('John', 'john@example.com') do |o|
+  o.on(:output) {|contact| puts contact.name }
+end
